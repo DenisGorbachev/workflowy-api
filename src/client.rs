9034ac4733f6e62910e240e957e35d3dc7395f0b
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-05-02 06:12:40 +0700

    fix: client

--- src/client.rs
+++ src/client.rs
@@ -31,13 +31,12 @@ impl Client {
         use ClientGetNodesError::*;
 
         let request = GetNodesRequestOwn::from(request);
-        let base = self.base.clone();
-        let path = NODES_PATH.to_string();
-        let nodes_url = handle!(base.join(path.as_str()), JoinNodesUrlFailed, base, path);
+        let path = NODES_PATH;
+        let url = handle!(self.base.join(path), JoinNodesUrlFailed, path);
         let http = handle!(reqwest::Client::builder().build(), BuildHttpClientFailed);
         let request_ref = request.as_ref();
-        let url_for_http = nodes_url.clone();
-        let url_for_error = nodes_url.clone();
+        let url_for_http = url.clone();
+        let url_for_error = url.clone();
         let request_for_error = request.clone();
         let response = http
             .get(url_for_http)
@@ -49,18 +48,18 @@ impl Client {
         let status = response.status();
 
         if status.is_success() {
-            let url_for_error = nodes_url.clone();
+            let url_for_error = url.clone();
             let request_for_error = request.clone();
             let response = response.json::<GetNodesResponse>().await;
             Ok(handle!(response, DecodeResponseFailed, url: url_for_error, request: request_for_error))
         } else {
-            let url_for_error = nodes_url.clone();
+            let url_for_error = url.clone();
             let request_for_error = request.clone();
             let body = response.text().await;
             let body = handle!(body, ReadErrorBodyFailed, status, url: url_for_error, request: request_for_error);
             Err(UnexpectedStatusInvalid {
                 status,
-                url: nodes_url,
+                url,
                 request,
                 body,
             })
@@ -80,8 +79,8 @@ impl From<Key> for Client {
 
 #[derive(Error, Debug)]
 pub enum ClientGetNodesError {
-    #[error("failed to join nodes path '{path}' to Workflowy API base URL '{base}'")]
-    JoinNodesUrlFailed { source: url::ParseError, base: Url, path: String },
+    #[error("failed to build URL for path: '{path}'")]
+    JoinNodesUrlFailed { source: url::ParseError, path: &'static str },
     #[error("failed to build HTTP client")]
     BuildHttpClientFailed { source: reqwest::Error },
     #[error("failed to send get nodes request to '{url}'")]
