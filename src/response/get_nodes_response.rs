b0ff73dbf6cb3fde30aedd5fac42baa4a7344e9f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-05-02 08:44:47 +0700

    fix: get_nodes_response

--- src/response/get_nodes_response.rs
+++ src/response/get_nodes_response.rs
@@ -2,7 +2,7 @@ use crate::Node;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Hash, Clone, Debug)]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "camelCase")]
 pub struct GetNodesResponse {
     pub nodes: Vec<Node>,
 }
