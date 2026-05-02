7ef64edf6ed8b8449152c942e49a3e32ca89eec4
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-05-02 12:30:16 +0700

    fix: integration_tests

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -12,9 +12,8 @@ static_env_var!(TEST_WORKFLOWY_API_KEY);
 
 #[tokio::test]
 async fn must_get_nodes() -> Result<(), Box<dyn Error>> {
-    let request = GetNodesRequest::from(ParentId::Root);
-
     let client = Client::new((*TEST_WORKFLOWY_API_KEY).clone())?;
+    let request = GetNodesRequest::from(ParentId::Root);
     let nodes = client.get_nodes(&request).await?;
     assert!(!nodes.nodes.is_empty());
 
