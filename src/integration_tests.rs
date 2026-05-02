20c29bd0fd5a899a1ad1c79c97ee484ba9938f60
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-05-02 13:05:33 +0700

    fix: integration_tests

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -1,16 +1,25 @@
-use crate::{Client, GetNodesRequest, ParentId};
+use crate::{Client, ClientGetNodesError, ClientNewError, GetNodesRequest, ParentId};
+use errgonomic::handle;
 use static_env_var::static_env_var;
 use std::clone::Clone;
-use std::error::Error;
+use thiserror::Error;
 
 static_env_var!(TEST_WORKFLOWY_API_KEY);
 
 #[tokio::test]
-async fn must_get_nodes() -> Result<(), Box<dyn Error>> {
-    let client = Client::new((*TEST_WORKFLOWY_API_KEY).clone())?;
-    let request = GetNodesRequest::from(ParentId::Root);
-    let nodes = client.get_nodes(&request).await?;
+async fn must_get_nodes() -> Result<(), MustGetNodesError> {
+    use MustGetNodesError::*;
+    let client = handle!(Client::new((*TEST_WORKFLOWY_API_KEY).clone()), ClientNewFailed);
+    let request = GetNodesRequest::<'static>::from(ParentId::Root);
+    let nodes = handle!(client.get_nodes(&request).await, GetNodesFailed, request);
     assert!(!nodes.nodes.is_empty());
-
     Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum MustGetNodesError {
+    #[error("failed to create Workflowy API client")]
+    ClientNewFailed { source: ClientNewError },
+    #[error("failed to get Workflowy nodes")]
+    GetNodesFailed { source: ClientGetNodesError, request: GetNodesRequest<'static> },
 }
