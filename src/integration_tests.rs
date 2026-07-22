8bed31a6a85e53270d43fb9c77ab6ce8f3282cf3
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-07-22 16:24:28 +0700

    fix: fnox

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -3,12 +3,12 @@ use errgonomic::handle;
 use static_env_var::static_env_var;
 use thiserror::Error;
 
-static_env_var!(TEST_WORKFLOWY_API_KEY);
+static_env_var!(WORKFLOWY_API_KEY);
 
 #[tokio::test]
 async fn must_get_nodes() -> Result<(), MustGetNodesError> {
     use MustGetNodesError::*;
-    let client = handle!(Client::new((*TEST_WORKFLOWY_API_KEY).clone()), ClientNewFailed);
+    let client = handle!(Client::new((*WORKFLOWY_API_KEY).clone()), ClientNewFailed);
     let request = GetNodesRequest::<'static>::from(ParentId::Root);
     let nodes = handle!(client.get_nodes(&request).await, GetNodesFailed, request);
     assert!(!nodes.nodes.is_empty());
